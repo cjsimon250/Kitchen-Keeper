5db1160a522004a8b687cb1ea1c4679d50d26280
CJ Simon <cjsimon250@outlook.com>
2023-05-31 13:40:59 -0500

    Altered user.strategy so that the user's company id is attatched to them

--- server/strategies/user.strategy.js
+++ server/strategies/user.strategy.js
@@ -1,42 +1,52 @@
-const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
-const encryptLib = require('../modules/encryption');
-const pool = require('../modules/pool');
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const encryptLib = require("../modules/encryption");
+const pool = require("../modules/pool");
 
 passport.serializeUser((user, done) => {
   done(null, user.id);
 });
 
-passport.deserializeUser((id, done) => {
-  pool
-    .query('SELECT * FROM "user" WHERE id = $1', [id])
-    .then((result) => {
-      // Handle Errors
-      const user = result && result.rows && result.rows[0];
+passport.deserializeUser(async (id, done) => {
+  try {
+    let result = await pool.query('SELECT * FROM "user" WHERE id = $1', [id]);
 
-      if (user) {
-        // user found
-        delete user.password; // remove password so it doesn't get sent
-        // done takes an error (null in this case) and a user
-        done(null, user);
-      } else {
-        // user not found
-        // done takes an error (null in this case) and a user (also null in this case)
-        // this will result in the server returning a 401 status code
-        done(null, null);
-      }
-    })
-    .catch((error) => {
-      console.log('Error with query during deserializing user ', error);
-      // done takes an error (we have one) and a user (null in this case)
-      // this will result in the server returning a 500 status code
-      done(error, null);
-    });
+    // Handle Errors
+    let user = result && result.rows && result.rows[0];
+
+    //Selecting the user's company id
+    const companyResult = await pool.query(
+      'SELECT id FROM "company" WHERE user_id = $1',
+      [id]
+    );
+
+    user = {
+      ...user,
+      companyId: companyResult.rows[0].id,
+    };
+
+    if (user) {
+      // user found
+      delete user.password; // remove password so it doesn't get sent
+      // done takes an error (null in this case) and a user
+      done(null, user);
+    } else {
+      // user not found
+      // done takes an error (null in this case) and a user (also null in this case)
+      // this will result in the server returning a 401 status code
+      done(null, null);
+    }
+  } catch (error) {
+    console.log("Error with query during deserializing user ", error);
+    // done takes an error (we have one) and a user (null in this case)
+    // this will result in the server returning a 500 status code
+    done(error, null);
+  }
 });
 
 // Does actual work of logging in
 passport.use(
-  'local',
+  "local",
   new LocalStrategy((username, password, done) => {
     pool
       .query('SELECT * FROM "user" WHERE username = $1', [username])
@@ -54,7 +64,7 @@ passport.use(
         }
       })
       .catch((error) => {
-        console.log('Error with query for user ', error);
+        console.log("Error with query for user ", error);
         // done takes an error (we have one) and a user (null in this case)
         // this will result in the server returning a 500 status code
         done(error, null);
